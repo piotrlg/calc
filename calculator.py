814ba82b75f288e3342a32419e5abc3026596614
piotrlg <piotrlg@gmail.com>
2019-01-16 16:20:23 +0100

    add  division func

--- calculator.py
+++ calculator.py
@@ -13,3 +13,7 @@ def subtract(first_term, second_term):
 
 def multiply(first_term, second_term):
     return first_term * second_term
+
+def div(first_term, second_term):
+    if second_term != 0:
+        return first_term / second_term
