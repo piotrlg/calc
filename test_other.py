54de380800c50d66dd90aafe2d7a4927d6590b13
piotrlg <piotrlg@gmail.com>
2019-01-19 18:43:31 +0100

    poprawione flake8

--- test_other.py
+++ test_other.py
@@ -4,7 +4,8 @@ tests for other.py
 
 import other
 
+
 class TestOther:
-    
+
     def test_add_suffix(self):
-        assert "Tosia bolek" == other.add_suffix ("bolek")
+        assert "Tosia bolek" == other.add_suffix("bolek")
