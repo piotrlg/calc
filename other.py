8bda3d87975c07a0b265fcb0d9b9427177832175
piotrlg <piotrlg@gmail.com>
2019-01-20 19:29:21 +0100

    samosia

--- other.py
+++ other.py
@@ -10,5 +10,11 @@ def add_prefix(s):
     """
     return s + " Tosia"
 
+def add_both(s):
+    """
+    Add prefix and suffix 's'
+    """
+    return s + " Samosia " + s
+
 if __name__ == "__main__":
     add_suffix("bolek")
